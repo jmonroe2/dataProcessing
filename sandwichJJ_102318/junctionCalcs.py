d9cbc38150a8ab9410741f6679cd863a56ee4f43
Jonathan Monroe <jonathan0monroe@gmail.com>
2018-10-31 00:28:05 -0500

    Finished number of junctions analysis
    Created data files out of oneNote data. Created figures for first comparision

--- sandwichJJ_102318/junctionCalcs.py
+++ sandwichJJ_102318/junctionCalcs.py
@@ -9,6 +9,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 plt.style.use('ggplot')
 import re
+from matplotlib.lines import Line2D
+
 
 def voltage_to_resistance_critCurr(v_obs_uv,bias_r=1E6):
     v_mon = 0.939E-3 # volts
@@ -72,76 +74,95 @@ def parse_input(input_list, regex_template, num_blocks,num_cols=None):
     else:
         return measurement_list
     return output
-                    
-    '''
-    for i,measure_block in enumerate(measurement_list):
-        for j,row in enumerate(measure_block):
-            while(len(row) < num_cols):
-                row.append(np.nan)
-            measure_block[j] = np.array(row)
-        measurement_list[i] = np.array(measure_block)
-    return measurement_list
-    print(measurement_list); return measurement_list;
-    '''
 ##END parse_input
     
     
-def make_plot(data, num_chips, ic_fun=None,x_labels=None):
-    '''
-    data: matrix of points to plot with index 0 is repe
-    '''
-    num_device_perChip = len(data[0])
-    avg = np.nanmean(data, axis=0)
-    var = np.nanvar(data, axis=0)
-    std = np.sqrt(var/num_device_perChip)
-    
-    for i,devices in enumerate(data):
-        indices = (i+1)*np.ones(num_device_perChip)
-        plt.plot(indices,devices, 'ko', alpha=0.2)
-    xs = np.arange(1,num_chips+1)
-    plt.errorbar(xs,avg, yerr=std, alpha=0.8,fmt='o',ms=15)
-    
-    ## legend
-    plt.title("Single SQuID")
-    if x_labels:
-        plt.xticks(xs,x_labels)
-    ax = plt.gca()
-    #ax.set_yscale('log')
-    ax.set_ylabel("$I_c$ [$\mu$ A]")
-    plt.show()  
-##END make_plot
-
-
-def main():
-    ## load data
-    dataFile = "sandwichJJ_102318_blt_q3Only.dat"
-    with open(dataFile) as open_file:
-        read = open_file.readlines()
-        open_file.close()
-
-    ## use regular expressions to extract 2 numbers (ints)
-    re_template = "((\d+\.?\d*)\suV)" # decimal number, then space then 'uV'
-    num_blocks = 5
-    voltage_list = parse_input(read, re_template, num_blocks=num_blocks,num_cols=4)
-    return voltage_list;
-    res, current = voltage_to_resistance_critCurr(voltage_list[0],bias_r=1E6)
-    
+def addToPlot_res_numJJ(resistance,color=None):
+    res = resistance/1E3
     ## parse into dict
     numJJ_template = np.array([[1, 1, 1, 0],
                                [2, 2, 2, 0],
                                [4, 4, 0, 0],
                                [8, 8, 0, 0],
-                               [8, 4, 2, 1],
+                               [15, 7, 3, 1],
                                [1, 0, 0, 0]])
     
-    numJunctions_dict = {}
+    ## separate all measurements by number of junctions
+    sorted_byNumJJ_dict = {} # key,value is num JJ, list of resistances
+    for chip_column in res:
+        num_rows = chip_column.shape[0]
+        num_cols = chip_column.shape[1]
+        for i in range(num_rows):
+            for j in range(num_cols):
+                num_jj = numJJ_template[i,j]
+                if not sorted_byNumJJ_dict.has_key(num_jj):
+                    sorted_byNumJJ_dict[num_jj] = []
+                sorted_byNumJJ_dict[num_jj].append(chip_column[i,j])
+        ##END loop through single block
+    ##END loop through all chip columns
     
-    ## plots
-    make_plot(current, num_blocks)
-    #make_plot(jj_list, num_chips)    
-    plt.title("Single (blue), Array (red)")
+    for num_jj in sorted_byNumJJ_dict.keys():
+        resistance_list = sorted_byNumJJ_dict[num_jj]
+        xs = num_jj*np.ones(len(resistance_list),dtype='int')
+        plt.scatter(xs,resistance_list,c='black',alpha=0.6,s=9)
+        
+        ## plot average
+        avg = np.nanmean(resistance_list)        
+        std = np.nanstd(resistance_list)
+        c = color if color else 'black'
+        plt.errorbar(num_jj,avg, yerr=std, alpha=0.8,fmt='o',ms=15,color=c)
+    ##END loop through number of junctions
+    plt.xlabel("Number of SQuIDs")
+    plt.ylabel("RT Resistance [k$\Omega$]")
+        
+##END plot_resistance_vs_numJunctions
+    
+    
+def plot_resistance_vs_numJunctions():
+    ## load data
+    #dataFile = "sandwichJJ_102318_blt_q3Only.dat"
+    dataFile_list = ["sandwichJJ_102318_pbj_q3.dat", \
+    "sandwichJJ_102318_pbj_q4.dat",\
+    "sandwichJJ_102318_blt_q3.dat",\
+    "sandwichJJ_102318_sub_q4.dat"]
+    for i,dataFile in enumerate(dataFile_list):
+        data_color = ['red', 'orange', 'purple', 'cyan'][i]
+        with open(dataFile) as open_file:
+            read = open_file.readlines()
+            open_file.close()
+    
+        ## use regular expressions to extract 2 numbers (ints)
+        re_template = "((\d+\.?\d*)\suV)" # decimal number, then space then 'uV'
+        num_blocks = 5
+        voltage_list = parse_input(read, re_template, num_blocks=num_blocks,num_cols=4)
+        res, current = voltage_to_resistance_critCurr(voltage_list,bias_r=1E6)
+        
+        ## do specific analysis
+        addToPlot_res_numJJ(res,color=data_color)
+    ##END loop through files
+    
+    ## make a custom legend
+    box_outline = dict(facecolor='white', alpha=0.8)
+    x_pos,y_pos = 9,500
+    gap = 100
+    plt.text(x_pos,y_pos,"PBJ Q3\n(No extra $O_2$) ", color='red',bbox=box_outline)  
+    plt.text(x_pos,y_pos-1*gap,"PBJ Q4\n(No extra $O_2$)", color='orange',bbox=box_outline)  
+    plt.text(x_pos,y_pos-2*gap,"BLT Q3\n(0.5 nm Al$O_x$)", color='purple',bbox=box_outline)  
+    plt.text(x_pos,y_pos-3*gap,"SUB Q4\n(ibid, $45^\circ$ evap)", color='cyan',bbox=box_outline)  
+    
+    ## more labels
+    plt.title("Sandwich Junction test 10/23/18")
+    plt.xlabel("Number of SQuIDs")
+    plt.ylabel("RT Resistance [k$\Omega$]")    
+    #plt.xlim(0,9)
+    #plt.ylim(0,200)
+##END plot_resistance_vs_numJunctions
+    
+    
+def main():
+    plot_resistance_vs_numJunctions()
 ##END main()
     
     
 if __name__ == '__main__':
-    ms = main()
+    main()
