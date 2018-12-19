ad16af2d105c1f5dbcb34113c11361269bda319e
John <jonathan0monroe@gmail.com>
2018-12-19 17:20:02 -0600

    Reorganized

---
+++ paramps/paramp091218/noiseAt2GHz_BNC_2.0GHz_-20.60dBm/analysis.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct  5 17:43:05 2018
+
+@author: jmonroe
+
+This script exists to parse smith data from VNA. 
+"""
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+
